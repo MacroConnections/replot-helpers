6b79cb22582dd98331a26c1a2e2e6e756ae6aa3b
Alisa Ono <alisao@mit.edu>
2018-02-02 01:15:34 -0500

    Added ComponentContainer, ColorTheme, SectionContainer

---
+++ index.js
@@ -0,0 +1,11 @@
+import ComponentContainer from "./src/ComponentContainers/ComponentContainer.jsx"
+import SectionContainer from "./src/SectionContainer.jsx"
+import ColorTheme from "./src/ColorTheme.js"
+// import DocTable from "./src/DocTable.jsx"
+
+export {
+  ComponentContainer,
+  SectionContainer,
+  ColorTheme,
+  // DocTable,
+}
